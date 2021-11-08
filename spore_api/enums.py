5f493ccf35d53387e3ce79a5f047e19b7890ea41
LEv145 <levxxx03@gmail.com>
2021-11-08 23:33:47 +0200

    Add `AssetSubtype` enum

--- spore_api/enums.py
+++ spore_api/enums.py
@@ -16,3 +16,43 @@ class ViewType(str, Enum):
     maxis_made = "MAXIS_MADE"
     random = "RANDOM"
     cute_and_creepy = "CUTE_AND_CREEPY"
+
+
+class AssetSubtype(int, Enum):
+    # CREATURE
+    animals = 501095725504
+    tribal = 501095388742
+    civ = 501083781321
+    space = 501095312644
+    captains = 501095829971
+
+    # BUILDING
+    city_hall = 0
+    house = 0
+    factory = 0
+    entertainment = 0
+
+    # VEHICLE
+    military = 0
+    economic = 0
+    religious = 0
+    colony = 0
+    land = 2597835946
+    air = 0
+    sea = 0
+    spaceship = 0
+
+    # ADVENTURE
+    attack = 0
+    collect = 0
+    defend = 0
+    explore = 0
+    puzzle = 0
+    quest = 0
+    socialize = 0
+    story = 0
+    template = 0
+    no_Genre = 0
+
+
+
