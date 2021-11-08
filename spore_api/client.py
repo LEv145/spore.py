ff29182736d468f1c9a856d02e2b4e2ad28549fd
LEv145 <levxxx03@gmail.com>
2021-11-08 20:55:42 +0200

    Add Client method

--- spore_api/client.py
+++ spore_api/client.py
@@ -5,8 +5,9 @@ from typing import TYPE_CHECKING, Any, Callable, Optional, Type, Union
 import aiohttp
 import xmltodict
 
+
 from .constants import BASE_URL
-from .abc import ABCBuilder
+from .enums import AssetType, ViewType
 from .builders import (
     StatsBuilder,
     CreatureBuilder,
@@ -23,22 +24,29 @@ from .builders import (
 if TYPE_CHECKING:
     from .models import (
         Stats,
-        Creature
+        Creature,
+        User,
+        Achievements,
+        AssetComments,
+        Assets,
+        Buddies,
+        SporecastAssets,
+        FullAsset
     )
 
 
 @dataclass
-class Builders():
-    stats: ABCBuilder
-    creature: ABCBuilder
-    user: ABCBuilder
-    assets: ABCBuilder
-    sporecasts: ABCBuilder
-    sporecast_assets: ABCBuilder
-    achievements: ABCBuilder
-    full_asset: ABCBuilder
-    asset_comments: ABCBuilder
-    buddies: ABCBuilder
+class Builders():  # TODO: Delete
+    stats: StatsBuilder
+    creature: CreatureBuilder
+    user: UserBuilder
+    assets: AssetsBuilder
+    sporecasts: SporecastsBuilder
+    sporecast_assets: SporecastAssetsBuilder
+    achievements: AchievementsBuilder
+    full_asset: FullAssetBuilder
+    asset_comments: AssetCommentsBuilder
+    buddies: BuddiesBuilder
 
 
 class SporeClient():
@@ -60,7 +68,7 @@ class SporeClient():
             full_asset=FullAssetBuilder(self._decoder),
             asset_comments=AssetCommentsBuilder(self._decoder),
             buddies=BuddiesBuilder(self._decoder)
-        )
+        )  # TODO: Delete
 
     def _init(self):
         self._base_url = BASE_URL
@@ -85,10 +93,125 @@ class SporeClient():
             await self.request(f"{self._base_url}/rest/stats")
         )
 
-    async def get_creature(self, creature_id: Union[int, str]) -> "Creature":
+    async def get_creature(
+        self,
+        creature_id: Union[int, str]
+    ) -> "Creature":
         return self._builders.creature.build(
             await self.request(f"{self._base_url}/rest/creature/{creature_id}")
         )
+
+    async def get_user_info(
+        self,
+        username: str
+    ) -> "User":
+        return self._builders.user.build(
+            await self.request(f"{self._base_url}/rest/user/{username}")
+        )
+
+    async def get_user_assets(
+        self,
+        username: str,
+        start_index: Union[int, str],
+        length: Union[int, str]
+    ) -> "Assets":
+        return self._builders.assets.build(
+            await self.request(
+                f"{self._base_url}/rest/assets/user/{username}/{start_index}/{length}"
+            )
+        )
+
+    async def get_user_sporecasts(
+        self,
+        username: str
+    ):
+        return self._builders.sporecasts.build(
+            await self.request(f"{self._base_url}/rest/sporecasts/{username}")
+        )
+
+    async def get_sporecast_assets(
+        self,
+        sporecast_id: Union[int, str],
+        start_index: Union[int, str],
+        length: Union[int, str]
+    ) -> "SporecastAssets":
+        return self._builders.sporecast_assets.build(
+            await self.request(
+                f"{self._base_url}/rest/assets/sporecast/{sporecast_id}/{start_index}/{length}"
+            )
+        )
+
+    async def get_user_achievements(
+        self,
+        username: str,
+        start_index: Union[int, str],
+        length: Union[int, str]
+    ) -> "Achievements":
+        return self._builders.achievements.build(
+            await self.request(
+                f"{self._base_url}/rest/achievements/{username}/{start_index}/{length}"
+            )
+        )
+
+    async def get_asset_info(
+        self,
+        asset_id: Union[int, str]
+    ) -> "FullAsset":
+        return self._builders.full_asset.build(
+            await self.request(
+                f"{self._base_url}/rest/asset/{asset_id}"
+            )
+        )
+
+    async def get_asset_comments(
+        self,
+        asset_id: Union[int, str],
+        start_index: Union[int, str],
+        length: Union[int, str]
+    ) -> "AssetComments":
+        return self._builders.asset_comments.build(
+            await self.request(
+                f"{self._base_url}/rest/comments/{asset_id}/{start_index}/{length}"
+            )
+        )
+
+    async def get_user_buddies(
+        self,
+        username: str,
+        start_index: Union[int, str],
+        length: Union[int, str]
+    ) -> "Buddies":
+        return self._builders.buddies.build(
+            await self.request(
+                f"{self._base_url}/rest/users/buddies/{username}/{start_index}/{length}"
+            )
+        )
+
+    async def get_user_subscribers(
+        self,
+        username: str,
+        start_index: Union[int, str],
+        length: Union[int, str]
+    ) -> "Buddies":
+        return self._builders.buddies.build(
+            await self.request(
+                f"{self._base_url}/rest/users/subscribers/{username}/{start_index}/{length}"
+            )
+        )
+
+    async def get_assets_searches(
+        self,
+        view_type: ViewType,
+        start_index: Union[int, str],
+        length: Union[int, str],
+        asset_type: Optional[AssetType] = None
+    ) -> "Assets":
+        if asset_type is None:
+            url = f"{self._base_url}/rest/assets/search/{view_type}/{start_index}/{length}"
+        else:
+            url = f"{self._base_url}/rest/assets/search/{view_type}/{start_index}/{length}/{asset_type}"
+
+        return self._builders.assets.build(await self.request(url))
 
     async def request(self, url: str) -> str:
         if self._session is None:
--- spore_api/enums.py
+++ spore_api/enums.py
@@ -6,3 +6,13 @@ class AssetType(str, Enum):
     creature = "CREATURE"
     vehicle = "VEHICLE"
     adventure = "ADVENTURE"
+
+
+class ViewType(str, Enum):
+    top_rated = "TOP_RATED"
+    top_rated_new = "TOP_RATED_NEW"
+    newest = "NEWEST"
+    featured = "FEATURED"
+    maxis_made = "MAXIS_MADE"
+    random = "RANDOM"
+    cute_and_creepy = "CUTE_AND_CREEPY"
