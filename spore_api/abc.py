a80a30a518bb97cfb1390bb93e601d58206c06bd
LEv145 <levxxx03@gmail.com>
2021-11-09 15:33:05 +0200

    Add `JSONSerializable`

--- spore_api/abc.py
+++ spore_api/abc.py
@@ -1,8 +1,10 @@
 from typing import Any, Callable
 from abc import ABC, abstractmethod
 
+from dataclass_wizard import JSONSerializable
 
-class ABCBuilder(ABC):
+
+class ABCBuilder(JSONSerializable, ABC):
     """Building data from raw to usable"""
     def __init__(self, decoder: Callable[[str], dict[str, Any]]) -> None:
         self._decoder = decoder
